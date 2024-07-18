c5a0ea84810112d10a5bc52c77eeb49a598998c3
Plamen Georgiev <plamen_gg@mail.bg>
2024-07-18 05:53:55 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/Registry_GasInstallations/RegistryGasInstallationsApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.Registry_GasInstallations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryGasInstallationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryGasInstallationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/Registry_GasInstallations/RegistryGasInstallationsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.Registry_GasInstallations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryGasInstallationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
